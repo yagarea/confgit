0b451b7351906e30b3c00c7e50f289689ee4c5c1
Jan <j.cerny.zdar@gmail.com>
2020-12-22 18:34:31 +0100

    comment fs util

--- fs_util.py
+++ fs_util.py
@@ -4,7 +4,10 @@ import yaml
 
 from log_util import *
 
+# FILE SYSTEM UTILITIES
 
+
+# make any given path as input relative to user's home directory
 def path_relative_home(raw_path):
     if raw_path[0] == "~":
         return raw_path
@@ -12,19 +15,22 @@ def path_relative_home(raw_path):
     return output.replace(str(path.expanduser("~")), "~")
 
 
-def path_absolute(raw_path):
+# make any given path as input absolute path from root directory
+def absolute_path(raw_path):
     return path.abspath(path.expanduser(path.expandvars(raw_path)))
 
 
+# translate origin file path to path of it's complementary file
 def path_in_repo(raw_path, config):
-    return path_absolute(raw_path).replace(str(path.expanduser("~")), config["repo_dir"])
+    return absolute_path(raw_path).replace(str(path.expanduser("~")), config["repo_dir"])
 
 
+# load config file to path on input and return parsed yaml
 def load_config(config_file_path):
     try:
         with open(config_file_path, 'r') as stream:
             raw_config = yaml.safe_load(stream)
-            raw_config["repo_dir"] = path_absolute(raw_config["repo_dir"])
+            raw_config["repo_dir"] = absolute_path(raw_config["repo_dir"])
             if raw_config["include"] is not None:
                 parsed = []
                 for i in raw_config["include"]:
@@ -44,26 +50,30 @@ def load_config(config_file_path):
         exit(1)
 
 
+# save config to yaml file to path on input
 def save_config(config, config_file_path):
     with open(config_file_path, "w") as f:
         yaml.dump(config, f, default_flow_style=False)
 
 
+# set current working directory to path on input
 def set_cwd_to(dir_path):
     chdir(path.realpath(dir_path))
 
 
+# return list of paths of all files in directory tree with root on path in input
 def mine_files(path_to_mine):
-    path_to_mine = path_absolute(path_to_mine)
+    path_to_mine = absolute_path(path_to_mine)
     if path.isfile(path_to_mine):
         return [path_to_mine]
     list_of_files = []
     for root, directories, files in walk(path_to_mine):
         for filename in files:
-            list_of_files.append(path_absolute(path.join(root, filename)))
+            list_of_files.append(absolute_path(path.join(root, filename)))
     return list_of_files
 
 
+# writes over source file to its destination file
 def write_file_to_other_file(source_path, destination_path: str):
     makedirs(path.dirname(destination_path), exist_ok=True)
     with open(source_path, "r") as source:
