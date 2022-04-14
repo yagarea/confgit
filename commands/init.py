24df77ffe311c739d1d40eb450faaf13117c2ecb
Generic <74469257+g3ner1c@users.noreply.github.com>
2022-04-14 12:14:20 -0400

    Whoops forgot to revert this

--- commands/init.py
+++ commands/init.py
@@ -6,19 +6,14 @@ from .util.sys_util import *
 
 #! not to be confused with __init__.py
 
-def init(repo_dir='.', config_path='.confgit.json'):
-
-    os.chdir(os.path.realpath(repo_dir))
-
-    if not os.path.isfile(config_path):
+def init(repo_dir, config_path):
+    set_cwd_to(repo_dir)
+    if not path.isfile(config_path):
         with open(config_path, "w") as new_config:
-            new_config.write( # im using json instead of yaml because json has a built-in module - g3ner1c
-                "{\n" +
-                f"    repo_dir: {os.path.normpath(os.getcwd())}\n" +
-                "    include: {\n" +
-                "    },\n" +
-                "    exclude: {\n" +
-                "    }\n"
+            new_config.write(
+                f"repo_dir: {path_relative_home(getcwd())}/\n" +
+                "include: NULL\n" +
+                "exclude: NULL"
             )
         cg_print(f"New config file generated: {config_path}")
     else:
